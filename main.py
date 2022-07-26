c688307a0134c62b61d2c17d1c27cfe2ccec016e
Phillip <philliplee515@gmail.com>
2022-07-26 11:03:53 +0900

    basic2

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from datetime import datetime
+# from datetime import datetime
 
-def get_date():
-    return f"Today is {datetime.now()}"
+# def get_date():
+#     return f"Today is {datetime.now()}"
+
+
+import random
+
+number_input = Element("number_input")
+def play_game(*args):
+    user_guess = number_input.value
+    machine_guess = random.randint(1, 50)
+    if int(user_guess) == machine_guess:
+        result.element.innerText = "You win!"
+    else:
+        result.element.innerText = f"You lost! The machine chose {machine_guess}!"
+    number_input.clear()
+
+    
